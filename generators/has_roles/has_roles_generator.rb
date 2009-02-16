ff4d92646e2a4e22725745a5a1e488b43c445803
Dean Strelau <dean@strelau.net>
2009-02-16 15:43:21 -0500

    Update generator to not require second param.

--- generators/has_roles/has_roles_generator.rb
+++ generators/has_roles/has_roles_generator.rb
@@ -1,9 +1,10 @@
-class HasRolesGenerator < Rails::Generator::NamedBase
+class HasRolesGenerator < Rails::Generator::Base
   def manifest
-      recorded_session = record do |m| 
-        unless options[:skip_migration] 
-          m.migration_template "migration.rb", "db/migrate" 
-        end 
+    recorded_session = record do |m|
+      unless options[:skip_migration]
+        m.migration_template "migration.rb", "db/migrate",
+          :migration_file_name => 'create_roles'
       end
+    end
   end
 end
