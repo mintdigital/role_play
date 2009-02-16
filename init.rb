a8254bd32e3317d0adf729ccf6e48ae6d5748ee2
Dean Strelau <dean@strelau.net>
2009-02-16 14:08:08 -0500

    Make tests run standalone (without enclosing Rails project).

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
+require 'active_record'
 require 'has_roles'
 require 'role'
 require 'role_assignment'
--- test/has_roles_test.rb
+++ test/has_roles_test.rb
@@ -1,36 +1,4 @@
-require 'test/unit'
-require '../../../../vendor/rails/activerecord/lib/active_record.rb'
-require '../init.rb' 
-
-ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :dbfile => ":memory:")
- 
-
-def setup_db
-  ActiveRecord::Schema.define(:version => 1) do
-    create_table :roleable_samples do |t|
-    end
-  end
-  
-  ActiveRecord::Schema.define(:version => 1) do
-    create_table :roles do |t|
-      t.string :name
-    end
-  end
-  
-  ActiveRecord::Schema.define(:version => 1) do
-    create_table :role_assignments do |t|
-      t.integer :roleable_id
-      t.string :roleable_type
-      t.integer :role_id
-    end
-  end
-end
-
-def teardown_db
-  ActiveRecord::Base.connection.tables.each do |table|
-    ActiveRecord::Base.connection.drop_table(table)
-  end
-end
+require File.join(File.dirname(__FILE__), 'test_helper')
 
 class RoleableSample < ActiveRecord::Base
   has_roles [:sample_role_1, :sample_role_2]
