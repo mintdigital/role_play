b90c378a77efcb70add90ccf198b3a0b09d6eaf4
Paul Fedory <paul.fedory@gmail.com>
2011-01-21 18:03:11 +0000

    Bumps version, adds more tests

--- test/migration_generator_test.rb
+++ test/migration_generator_test.rb
@@ -5,9 +5,16 @@ class MigrationGeneratorTest < Rails::Generators::TestCase
   tests RolePlayMigrationGenerator
   destination File.expand_path("../tmp", File.dirname(__FILE__))
   setup :prepare_destination
-  
+
   test "migration file is created" do
     run_generator
-    assert_migration "db/migrate/create_roles.rb"
+    assert_migration "db/migrate/create_roles.rb" do |mig|
+      assert_class_method :up, mig do |upmeth|
+        assert_match /create_table/, upmeth
+      end
+      assert_class_method :down, mig do |downmeth|
+        assert_match /drop_table/, downmeth
+      end
+    end
   end
 end
