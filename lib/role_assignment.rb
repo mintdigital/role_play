07c8f916a5ae02b7dc7a3ea6bbb641b432bc1d47
Paul Fedory <paul@mintdigital.com>
2013-08-06 00:20:05 +0100

    Fixes outdated validates presence requirement

--- lib/role_assignment.rb
+++ lib/role_assignment.rb
@@ -2,5 +2,5 @@ class RoleAssignment < ActiveRecord::Base
   belongs_to :roleable, :polymorphic => true
   belongs_to :role
   validates_uniqueness_of :role_id, :scope => [:roleable_id, :roleable_type]
-  validates_presence_of :roleable, :roleable_type, :role
+  validates_presence_of :roleable, :role
 end
